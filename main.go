2e15eb0e649f6ada6309784709d78945e56e43e9
dsdf <dasdgdafg@cock.li>
2017-04-20 19:57:33 -0400

    hello world server

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "time"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    time.Sleep(time.Second * 5)
+    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":8080", nil)
+}

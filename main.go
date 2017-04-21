8ce4cdc874280d91886770d943b32839a5af521c
dsdf <dasdgdafg@cock.li>
2017-04-20 21:39:52 -0400

    go fmt

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "time"
+	"fmt"
+	"net/http"
+	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    time.Sleep(time.Second * 5)
-    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	time.Sleep(time.Second * 5)
+	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
